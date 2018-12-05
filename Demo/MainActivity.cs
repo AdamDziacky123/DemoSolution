1c8480c86864157083048ee294ae850e3ce35dd6
Adam Dziacky <adam.dziacky@studentstc.sk>
2018-12-05 15:00:37 +0100

    Fuck this shit

---
+++ Demo/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Demo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        Button btnContinue;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnContinue = FindViewById<Button>(Resource.Id.button1);
+            btnContinue.Click += ContinueBtnClicked;
+        }
+
+
+        void ContinueBtnClicked(object sender, EventArgs args)
+        {
+            var intent = new Intent(this, typeof(TestActivity));
+            this.StartActivity(intent);
+        }
+
+
+    }
+}
